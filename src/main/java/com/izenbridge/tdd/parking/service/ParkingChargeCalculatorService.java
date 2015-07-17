ad3f4522e1339cc1bdd4b671999568993a319408
agilitydelivers <sunjaykumar@gmail.com>
2015-07-17 13:52:17 +0530

    version 2 - first test passed

--- src/main/java/com/izenbridge/tdd/parking/service/ParkingChargeCalculatorService.java
+++ src/main/java/com/izenbridge/tdd/parking/service/ParkingChargeCalculatorService.java
@@ -5,8 +5,12 @@ import java.util.Date;
 public class ParkingChargeCalculatorService {
 
 	public int calculateParkingCharge(Date inTime, Date outTime) {
-		// TODO Auto-generated method stub
-		return 0;
+		int parkingTimeInMinutes = (int) ((outTime.getTime() - inTime.getTime())/60000);
+		if (parkingTimeInMinutes <= 60) {
+			return 20;
+		}
+
+		return 10000;	// By default, return a ridiculously high parking charge 
 	}
 
 }
